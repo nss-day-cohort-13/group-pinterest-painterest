994054ba7a7b9788a27043f95f90e41d026b6113
Mark Easterling <marke.easterling@gmail.com>
2016-06-07 15:48:26 -0500

    routing added for new board button

--- src/app/user-home/user.config.js
+++ src/app/user-home/user.config.js
@@ -6,4 +6,10 @@ angular.module('app')
         controllerAs: 'user',
         templateUrl: '/app/user-home/user.index.html'
       })
+      $routeProvider
+      .when('/newBoard', {
+        controller: 'UserCtrl',
+        controllerAs: 'user',
+        templateUrl: '/app/user-home/board.form.html'
+      })
   })
