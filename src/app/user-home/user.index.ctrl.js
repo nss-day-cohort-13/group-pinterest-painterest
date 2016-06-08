3b3cbf74d5dcf764968fe535ed0efac00b30d905
Mark Easterling <marke.easterling@gmail.com>
2016-06-08 10:36:14 -0500

    http get firebase data

--- src/app/user-home/user.index.ctrl.js
+++ src/app/user-home/user.index.ctrl.js
@@ -2,11 +2,11 @@ angular.module('app')
   .controller('UserCtrl', function ($http) {
   	const user = this
 
-  	$http({
-  		method: 'GET',
-  		url: 'https://project-8469292309614253139.firebaseio.com/.json'
-  	}).then((response) => {
-  		console.log(response.data)
-  		})
-
+  $http({
+		method: 'GET',
+		url: 'https://project-8469292309614253139.firebaseio.com/.json'
+  })
+		.then((response) => {
+			console.log(response.data)
+		})
   })
