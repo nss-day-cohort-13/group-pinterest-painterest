3e740657e68b031510d448c05404d521d2ab477f
Cory Tohill <corytohill@gmail.com>
2016-06-08 10:55:16 -0500

    removed unnecessary code

--- src/app/auth/logout.ctrl.js
+++ src/app/auth/logout.ctrl.js
@@ -1,6 +1,5 @@
 angular.module('app')
   .controller('LogoutCtrl', function (AuthFactory, $scope, $location) {
-    const auth = this;
 
     $scope.logout = function () {
       AuthFactory.logout()
